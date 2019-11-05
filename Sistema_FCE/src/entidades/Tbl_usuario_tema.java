f387582afa0dadb687c9af75a6843bb84291909a
Viclicioso11 <victorabud11@gmail.com>
2019-11-04 21:40:51 -0600

    para hacer pull

--- Sistema_FCE/src/entidades/Tbl_usuario_tema.java
+++ Sistema_FCE/src/entidades/Tbl_usuario_tema.java
@@ -1,18 +1,18 @@
 package entidades;
 
 public class Tbl_usuario_tema {
-<<<<<<< HEAD
+
 	
 	//ATRIBUTOS
 	private int id_usuario_tema;
-=======
+
 	private int id;
->>>>>>> 382f651c1ff4284f34debc24e82b01c2a1cf64ce
+
 	private int id_usuario;
 	private int id_tema;
 	
 	
-<<<<<<< HEAD
+
 
 	//METODOS
 	public int getId_usuario_tema() {
@@ -20,13 +20,12 @@ public class Tbl_usuario_tema {
 	}
 	public void setId_usuario_tema(int id_usuario_tema) {
 		this.id_usuario_tema = id_usuario_tema;
-=======
+	}
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
->>>>>>> 382f651c1ff4284f34debc24e82b01c2a1cf64ce
 	}
 	public int getId_usuario() {
 		return id_usuario;
@@ -40,12 +39,6 @@ public class Tbl_usuario_tema {
 	public void setId_tema(int id_tema) {
 		this.id_tema = id_tema;
 	}
-<<<<<<< HEAD
-	
-	
-	
-	
 
-=======
->>>>>>> 382f651c1ff4284f34debc24e82b01c2a1cf64ce
+
 }
